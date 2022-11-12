647ec6ed6993f4ae3306bfd1f05e46afa5b6ec56
panda1 <blueblue0011@gmail.com>
2022-11-12 08:05:44 -0800

    added structural, and a few endpoints

--- app.py
+++ app.py
@@ -6,12 +6,67 @@ import dbhelpers as dh
 import apihelpers as a
 import json
 import dbcreds as d
+import requests
 
 
 app = Flask(__name__)
 
+@app.post('/api/client')
+def new_client():
 
+    salt = uuid4().hex
+    valid_check=a.check_endpoint_info(request.json, ['username', 'email', 'password', 
+    'api_key'])
+    if(valid_check != None):
+        return make_response(json.dumps(valid_check, default=str), 400)
+    
+    
+    
 
+    result = dh.run_statement('CALL new_client(?,?,?,?,?)', [request.json.get('username'), request.json.get('email'),
+    request.json.get('password'), salt, request.json.get('api_key')])
+
+    if(type(result) == list):
+        return make_response(json.dumps(result, default=str), 200)
+    else:
+        return make_response(json.dumps(result, default=str), 400)
+
+@app.post('/api/client-login')
+def client_login():
+    token = uuid4().hex
+    is_valid=a.check_endpoint_info(request.json, ['email', 'password'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+    result = dh.run_statement('CALL client_login(?,?,?)', [request.json.get('email'),
+    request.json.get('password'), token])
+    if(type(result)==list):
+        return make_response(json.dumps(result, default=str), 200)
+    else:
+        return make_response(json.dumps(result, default=str), 400)
+
+@app.post('/api/inventory')
+def client_inventory():
+
+    myrecipes=requests.get('https://api.guildwars2.com/v2/account/recipes', params={
+        "access_token": "276AFC0E-F2CD-8B49-9349-F8A60015193379CDF517-3757-4F10-BB05-C5ED8FC493C4"
+    })
+
+    my_inventory_coin =requests.get('https://api.guildwars2.com/v2/account/wallet', params={
+    "access_token": "276AFC0E-F2CD-8B49-9349-F8A60015193379CDF517-3757-4F10-BB05-C5ED8FC493C4"})
+
+    is_valid = a.check_endpoint_info(request.json, ['copper', 'recipes'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+
+    result = dh.run_statement('CALL client_inventory_ingredients(?,?)', [my_inventory_coin[0]['value'], 
+    myrecipes[:10]])
+
+    if(type(result)==list):
+        return make_response(json.dumps(result, default=str), 200)
+    else:
+        return make_response(json.dumps(result, default=str), 400)    
+
+    # print(my_inventory_coin.json())
 
     
 
---
+++ mvp.sql
@@ -0,0 +1,204 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: m.v.p
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.6.9-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `salt` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `api_key` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `client_un` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `client_recipe_owned`
+--
+
+DROP TABLE IF EXISTS `client_recipe_owned`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client_recipe_owned` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `recipes_owned_id` int(10) unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_recipe_owned_FK` (`recipes_owned_id`),
+  CONSTRAINT `client_recipe_owned_FK` FOREIGN KEY (`recipes_owned_id`) REFERENCES `recipe_bank` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `client_session`
+--
+
+DROP TABLE IF EXISTS `client_session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client_session` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `client_id` int(10) unsigned NOT NULL,
+  `token` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `client_session_un` (`token`),
+  KEY `client_session_FK` (`client_id`),
+  CONSTRAINT `client_session_FK` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ingredients`
+--
+
+DROP TABLE IF EXISTS `ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ingredients` (
+  `id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `recipe_bank`
+--
+
+DROP TABLE IF EXISTS `recipe_bank`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipe_bank` (
+  `id` int(10) unsigned NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `type` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `item_output_id` int(10) unsigned NOT NULL,
+  `output_count` int(10) unsigned NOT NULL,
+  `crafting_time_in_ms` int(10) unsigned DEFAULT NULL,
+  `crafting_skills` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `needed_ingredient` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `ingredient_id` int(10) unsigned NOT NULL,
+  `image_url` varchar(400) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `recipe_bank_FK` (`ingredient_id`),
+  CONSTRAINT `recipe_bank_FK` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'm.v.p'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `client_login` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `client_login`(email_input varchar(100), password_input varchar(100), token_input varchar(100))
+    MODIFIES SQL DATA
+BEGIN
+	
+	
+	insert into client_session (token, created_at, client_id)
+	VALUES
+	(token_input, now(), (SELECT id  FROM client
+	WHERE email=email_input AND password=PASSWORD(CONCAT(password_input, (select salt where email = email_input)))));
+	
+	
+	select ROW_COUNT(), last_insert_id(), CONVERT(token_input USING utf8);
+	commit;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `client_recipes` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `client_recipes`(recipes_owned_id_input int unsigned)
+    MODIFIES SQL DATA
+BEGIN
+	INSERT INTO client_recipe_owned(recipes_owned_id, created_at)
+	VALUES
+	(recipes_owned_id_input, now());
+	SELECT ROW_COUNT();
+	COMMIT;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `new_client` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `new_client`(username_input varchar(100), email_input varchar(100), password_input varchar(100), salt_input varchar(100),
+api_key_input varchar(100))
+    MODIFIES SQL DATA
+BEGIN
+	INSERT INTO client (username, email, password, salt, api_key, created_at)
+	VALUES
+	(username_input, email_input, PASSWORD(concat(password_input, salt_input)), salt_input, api_key_input, now());
+	SELECT ROW_COUNT();
+	COMMIT;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-12  8:05:16
